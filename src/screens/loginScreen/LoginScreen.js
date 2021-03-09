a0fc0490065d9e7e9ed578a1a5b606df0ce4e255
pencil <penciljamnoreply@gmail.com>
2021-03-09 00:39:30 +0000

    changes done

--- src/screens/loginScreen/LoginScreen.js
+++ src/screens/loginScreen/LoginScreen.js
@@ -25,13 +25,13 @@ const LoginScreen = () => {
    return (
       <div className='login'>
          <div className='login__container'>
-            <h2>Youtube Clone</h2>
+            <h2>DevTube</h2>
             <img
                src='http://pngimg.com/uploads/youtube/youtube_PNG2.png'
                alt=''
             />
-            <button onClick={handleLogin}>Login With google</button>
-            <p>This Project is made using YOUTUBE DATA API</p>
+            <button onClick={handleLogin}>LOGIN USING GOOGLE</button>
+            <p>This WebApp is made using YOUTUBE DATA API</p>
          </div>
       </div>
    )
